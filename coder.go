4f09090b183385505f3c566aef453ea58fea8d2a
Willem Victor <wimpievictor@gmail.com>
2019-08-31 14:01:22 +0200

    Wrote tests for encoding

--- coder.go
+++ coder.go
@@ -1,21 +1,29 @@
 package main
 
-import "strings"
-
 const (
 	alphabet string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_"
 	base     uint64 = uint64(len(alphabet))
 )
 
+func reverse(s string) string {
+	runes := []rune(s)
+
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+
+	return string(runes)
+}
+
 // EncodeNumber takes a base 10 number and
 // converts it to a shortened representation.
 func EncodeNumber(num uint64) string {
-	sb := make([]string, 0)
+	runes := make([]rune, 0)
 
 	for num > 0 {
-		sb = append(sb, string(alphabet[num%base]))
+		runes = append(runes, rune(alphabet[num%base]))
 		num /= base
 	}
 
-	return strings.Join(sb, "")
+	return reverse(string(runes))
 }
--- coder_test.go
+++ coder_test.go
@@ -1,15 +1,50 @@
 package main
 
-
 import (
 	"testing"
 )
 
-func TestEncoder(t *testing.T) {
-	num := 123
-	short := EncodeNumber(num)
+type data struct {
+	num      uint64
+	expected string
+	actual   string
+}
 
-	if short != "abc" {
-		t.Errorf("")
+func TestReverse(t *testing.T) {
+	s := "1234567890"
+	reversed := reverse(s)
+
+	if reversed != "0987654321" {
+		t.Error("String was not reversed properly. Expected:", s, "Actual", reversed)
+	}
+}
+
+func TestEncoder(t *testing.T) {
+	nums := [3]data{
+		data{
+			num:      10,
+			expected: "k",
+			actual:   "",
+		},
+		data{
+			num:      64,
+			expected: "ba",
+			actual:   "",
+		},
+		data{
+			num:      4500,
+			expected: "bgu",
+			actual:   "",
+		},
+	}
+
+	for i := 0; i < len(nums); i++ {
+		nums[i].actual = EncodeNumber(nums[i].num)
+	}
+
+	for _, num := range nums {
+		if num.expected != num.actual {
+			t.Error("Expected:", num.expected, "Actual:", num.actual)
+		}
 	}
 }
