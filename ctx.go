d7143b7db3c10780a9698490a9a636e44b01727f
Willem Victor <wimpievictor@gmail.com>
2019-09-08 16:42:58 +0200

    Changed context key to unexported

--- ctx.go
+++ ctx.go
@@ -1,20 +1,20 @@
 package main
 
-// ContextKey is the package wide type
+// contextKey is the package wide type
 // for value keys in the request context.
-type ContextKey int
+type contextKey int
 
 const (
 	// AuthUserKey is the key for the current
 	// request scoped authenticated user.
-	AuthUserKey ContextKey = iota
+	AuthUserKey contextKey = iota
 
 	// LogKey is the key for the current logging
 	// context, which contains request scoped
 	// fields, such as correlation ID.
-	LogKey ContextKey = iota
+	LogKey contextKey = iota
 
 	// CorrelationKey is the key for the current
 	// request's correlation ID.
-	CorrelationKey ContextKey = iota
+	CorrelationKey contextKey = iota
 )
